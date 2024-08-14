d854e745bbda0dd80b51a75d0166899e5183edc7
us4c0d3 <jwseok312@gmail.com>
2024-08-14 14:04:28 +0900

    feat(Ping): on_ready 로그 추가

--- src/cogs/Ping.py
+++ src/cogs/Ping.py
@@ -7,6 +7,10 @@ class Ping(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Ping.py is ready')
+
     # 봇의 핑을 pong! 이라는 메세지와 함께 전송한다. latency는 일정 시간마다 측정됨에 따라 정확하지 않을 수 있다.
     @app_commands.command(name="ping", description="봇의 핑을 확인합니다.")
     async def ping(self, interaction: discord.Interaction) -> None:
