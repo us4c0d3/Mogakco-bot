cf394fa6d863859fa246c86c8c8287ae14abc0ea
us4c0d3 <us4c0d3@gmail.com>
2024-09-01 23:08:00 +0900

    feat(Alert): 알림 관련 기능 추가
    알림 관련 타임라인
    * 18:50 투표 마감 10분 전 알림
    * 19:20 투표자 취합
    * 19:30 20시 참가자 30분 전 알림
    * 20:10 지각자 알림
    * 21:30 22시 참가자 30분 전 알림
    * 22:10 지각자 알림

--- src/cogs/Vote.py
+++ src/cogs/Vote.py
@@ -9,9 +9,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-POLL_CHANNEL_ID = os.getenv('POLL_CHANNEL_ID')
+VOTE_CHANNEL_ID = os.getenv('VOTE_CHANNEL_ID')
 VOICE_CHANNEL_ID = os.getenv('VOICE_CHANNEL_ID')
-NOTIFICATION_CHANNEL_ID = os.getenv('NOTIFICATION_CHANNEL_ID')
+ATTENDANCE_CHANNEL_ID = os.getenv('ATTENDANCE_CHANNEL_ID')
 TEST_POLL_CHANNEL_ID = os.getenv('TEST_POLL_CHANNEL_ID')
 TEST_VOICE_CHANNEL_ID = os.getenv('TEST_VOICE_CHANNEL_ID')
 TEST_GUILD_ID = os.getenv('TEST_GUILD_ID')
@@ -27,47 +27,24 @@ class Vote(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
 
-        # self.vote_channel_id = int(TEST_POLL_CHANNEL_ID)
-        self.vote_channel_id = int(POLL_CHANNEL_ID)
+        self.vote_channel_id = int(TEST_POLL_CHANNEL_ID)
+        # self.vote_channel_id = int(VOTE_CHANNEL_ID)
         self.vote_channel = None
 
-        # self.voice_channel_id = int(TEST_VOICE_CHANNEL_ID)
-        self.voice_channel_id = int(VOICE_CHANNEL_ID)
-        self.voice_channel = None
-
-        self.notification_channel_id = int(NOTIFICATION_CHANNEL_ID)
-        self.notification_channel = None
-
         self.poll_message = None
-        self.eight_to_ten_voters = []
-        self.ten_to_twelve_voters = []
 
     @commands.Cog.listener()
     async def on_ready(self):
         self.vote_channel = self.bot.get_channel(self.vote_channel_id)
-        self.voice_channel = self.bot.get_channel(self.voice_channel_id)
-        self.notification_channel = self.bot.get_channel(self.notification_channel_id)
 
         if self.vote_channel:
             logging.info(f'Default vote channel set to: {self.vote_channel.name} (ID: {self.vote_channel.id})')
         else:
             logging.warning(f'Channel with ID {self.vote_channel_id} not found.')
 
-        if self.voice_channel:
-            logging.info(f'Default voice channel set to: {self.voice_channel.name} (ID: {self.voice_channel.id})')
-        else:
-            logging.warning(f'Channel with ID {self.voice_channel_id} not found.')
-
-        if self.notification_channel:
-            logging.info(f'Default notification channel set to: '
-                         f'{self.notification_channel.name} (ID: {self.notification_channel.id})')
-        else:
-            logging.warning(f'Channel with ID {self.notification_channel_id} not found.')
-
         self.create_vote.start()
         self.get_voters.start()
-        self.alert_eight_to_ten.start()
-        self.alert_ten_to_twelve.start()
+
         logging.info('Vote.py is ready')
 
     @tasks.loop(time=time(hour=9, minute=0, second=0, tzinfo=KST))
@@ -87,28 +64,34 @@ class Vote(commands.Cog):
             poll.add_answer(text="22시 ~ 24시 참가", emoji='✅')
 
             # await interaction.response.send_message(poll=poll)
+            # self.poll_message = await interaction.original_response()
             self.poll_message = await self.vote_channel.send(poll=poll)
             logging.info(f'poll message: {self.poll_message}')
+
         except Exception as e:
             logging.error(e)
 
-    # 19:30 투표자 취합
-    @tasks.loop(time=time(hour=19, minute=30, second=0, tzinfo=KST))
-    # @app_commands.command(name='getvoters')
+    # 19:20 투표자 취합
+    @tasks.loop(time=time(hour=19, minute=20, second=0, tzinfo=KST))
+    # @app_commands.command(name='getvoters', description='투표자 취합')
     async def get_voters(self) -> None:
         try:
             message = await self.vote_channel.fetch_message(self.poll_message.id)
             poll = message.poll
             logging.info(f'poll answers: {poll.answers}')
 
-            self.eight_to_ten_voters = [voter async for voter in poll.get_answer(id=1).voters()]
-            self.ten_to_twelve_voters = [voter async for voter in poll.get_answer(id=2).voters()]
-            logging.info(f'8 ~ 10 투표자: {self.eight_to_ten_voters}')
-            logging.info(f'10 ~ 12 투표자: {self.ten_to_twelve_voters}')
+            eight_to_ten_voters = [voter async for voter in poll.get_answer(id=1).voters()]
+            ten_to_twelve_voters = [voter async for voter in poll.get_answer(id=2).voters()]
+            logging.info(f'20 ~ 22 투표자: {eight_to_ten_voters}')
+            logging.info(f'22 ~ 24 투표자: {ten_to_twelve_voters}')
 
-            # await interaction.response.send_message(f'8 ~ 10 투표자: {self.eight_to_ten_voters}\n'
-            #                                         f'10 ~ 12 투표자: {self.ten_to_twelve_voters}',
+            # await interaction.response.send_message(f'20 ~ 22 투표자: {eight_to_ten_voters}\n'
+            #                                         f'22 ~ 24 투표자: {ten_to_twelve_voters}',
             #                                         ephemeral=True)
+
+            alert_cog = self.bot.get_cog('Alert')
+            if alert_cog:
+                alert_cog.update_voters(eight_to_ten_voters, ten_to_twelve_voters)
 
         except Exception as e:
             logging.error(e)
@@ -122,45 +105,6 @@ class Vote(commands.Cog):
             await message.end_poll()
             logging.info('투표를 종료시켰습니다.')
             await interaction.response.send_message('투표를 종료시켰습니다.', ephemeral=True)
-        except Exception as e:
-            logging.error(e)
-
-    # @app_commands.command(name='alert_eight')
-    @tasks.loop(time=time(hour=20, minute=10, second=0, tzinfo=KST))
-    async def alert_eight_to_ten(self) -> None:
-        try:
-            if self.voice_channel is None or self.notification_channel is None:
-                logging.warning("Voice channel 또는 notification channel이 설정되지 않았습니다.")
-                return
-
-            voice_channel_members = self.voice_channel.members
-            logging.info(f'Voice channel members: {voice_channel_members}')
-
-            not_in_voice = [member for member in self.eight_to_ten_voters if member not in voice_channel_members]
-            logging.info(f'Not in voice channel members: {not_in_voice}')
-
-            mentions = ' '.join([f'<@{member.id}>' for member in not_in_voice])
-            if len(not_in_voice) != 0:
-                await self.notification_channel.send(f'{mentions} 20시 10분 입니다. 혹시 깜빡하셨나요?')
-        except Exception as e:
-            logging.error(e)
-
-    @tasks.loop(time=time(hour=22, minute=10, second=0, tzinfo=KST))
-    async def alert_ten_to_twelve(self) -> None:
-        try:
-            if self.voice_channel is None or self.notification_channel is None:
-                logging.warning("Voice channel 또는 notification channel이 설정되지 않았습니다.")
-                return
-
-            voice_channel_members = self.voice_channel.members
-            logging.info(f'Voice channel members: {voice_channel_members}')
-
-            not_in_voice = [member for member in self.ten_to_twelve_voters if member not in voice_channel_members]
-            logging.info(f'Not in voice channel members: {not_in_voice}')
-
-            mentions = ' '.join([f'<@{member.id}>' for member in not_in_voice])
-            if len(not_in_voice) != 0:
-                await self.notification_channel.send(f'{mentions} 22시 10분 입니다. 혹시 깜빡하셨나요?')
         except Exception as e:
             logging.error(e)
 
