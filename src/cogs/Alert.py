7f818fc8b11225919d7d9389f0cf4b0345883585
us4c0d3 <jwseok312@gmail.com>
2024-11-07 17:43:36 +0900

    feat: 누적 접속 시간 출력 형식 수정

--- src/cogs/Alert.py
+++ src/cogs/Alert.py
@@ -106,14 +106,14 @@ class Alert(commands.Cog):
 
         if time(19, 31, 0) <= now.time() <= time(23, 59, 59):
             if before.channel is None and after.channel is not None:
-                self.join_time[member] = now
+                self.join_time[member]: datetime = now
                 logging.info(f'{member.display_name} 님이 {now}에 통화방에 참가했습니다')
 
         if before.channel is None and after.channel is not None:
             if member not in self.voice_times:
                 self.voice_times[member] = timedelta(0)
             self.voice_times[member] += now - self.join_time[member]
-            formatted_time = self.voice_times[member].strftime("%H시간 %M분")
+            formatted_time = self.format_time(self.voice_times[member])
             logging.info(f'{member.display_name} 님이 통화방에서 퇴장했습니다. 누적 접속 시간: {formatted_time}')
             await self.attendance_channel.send(f'<@{member.id}> 님의 현재까지 통화방 누적 접속 시간: {formatted_time}')
 
@@ -184,6 +184,11 @@ class Alert(commands.Cog):
         self.voice_times.clear()
         self.two_hours_members.clear()
         self.attend_voters.clear()
+
+    def format_time(self, delta: timedelta) -> str:
+        hour = delta.days * 24 + delta.seconds // 3600
+        minute = (delta.seconds % 3600) // 60
+        return f'{hour}시간 {minute}분'
 
 
 async def setup(bot) -> None:
