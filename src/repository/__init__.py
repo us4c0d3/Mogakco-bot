67976416f692dfe149b7d3ac28b4e7de711dd27a
us4c0d3 <jwseok312@gmail.com>
2024-11-29 17:06:01 +0900

    refactor($repository): config를 상위 __init__에서 처리하도록 수정

--- src/repository/__init__.py
+++ src/repository/__init__.py
@@ -1,3 +1,14 @@
+import os
+
 from dotenv import load_dotenv
 
 load_dotenv("../../db.env")
+
+CONFIG = {
+    'host': os.getenv('DB_HOST'),
+    'port': int(os.getenv('DB_PORT', 3306)),
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD'),
+    'database': os.getenv('DB_DATABASE'),
+    'charset': os.getenv('DB_CHARSET', 'utf8mb4'),
+}
